c3dd784658e3b9b86d15114eed5f3cc88971ed72
Nerio <nerio.hdiaz@gmail.com>
2023-05-01 23:04:06 -0400

    Added Functionalities and Tests

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from fastapi import FastAPI
+import hashlib
+from typing import Annotated
+import redis
+from fastapi import Body, FastAPI
+from typing import List, Dict
+
+import aioredis
+import json
 
 app = FastAPI()
+
+redis_cache = redis.Redis(host="localhost", port=6379, db=0)
 
 
 @app.get("/")
@@ -8,3 +17,23 @@ async def root():
     return {"message": "I'am root :)"}
 
 
+@app.post('/solution', response_model=float | dict[str, str])
+async def process_orders(orders: Annotated[List, Body(embed=False)],
+                         criterion: Annotated[str, Body(embed=False)]) -> float | Dict[str, str]:
+    if criterion is None or criterion not in ["completed", "canceled", "pending", "all"]:
+        return {"message": "malformed input"}
+
+    result = 0
+    js = dict({"orders": orders, "criterion": criterion})
+    key = hashlib.md5(json.dumps(js).encode('utf-8')).hexdigest()
+    if redis_cache.exists(key):
+        return float(redis_cache.get(key))
+
+    for order in orders:
+        if float(order["price"]) <= 0 or order["status"] not in ["completed", "canceled", "pending"]:
+            return {"message": "malformed input"}
+        if criterion == "all" or order["status"] == criterion:
+            result = result + order["price"]
+    result = round(result, 2)
+    redis_cache.set(key, result)
+    return result
